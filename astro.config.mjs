3bd356e836a7b464a1b09d7ab065868832e3e230
Rokas Mikalkėnas <rokas.mikalkenas@hostinger.com>
2026-05-04 21:03:39 +0300

    add og meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://zagare.today",
   integrations: [react()],
 
   build: {
