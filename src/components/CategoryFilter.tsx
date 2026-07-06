1a71f4f6794aa86797be2ba8b6a1969719280fa4
Rokas Mikalkėnas <rokas.mikalkenas@hostinger.com>
2026-07-06 12:10:54 +0300

    Design adjustments

--- src/components/CategoryFilter.tsx
+++ src/components/CategoryFilter.tsx
@@ -11,14 +11,16 @@ export default function CategoryFilter({ active, onToggle, onReset }: Props) {
   const allOn = active.size === CATEGORY_KEYS.length;
 
   return (
-    <aside className="md:col-span-4 lg:col-span-3 hidden md:flex md:order-1 flex-col">
-      <div className="flex justify-end mb-4 min-h-[14px]">
-        {!allOn && (
-          <button type="button" onClick={onReset} className="meta-link">
-            Rodyti viską
-          </button>
-        )}
-      </div>
+    <aside className="relative md:col-span-4 lg:col-span-3 hidden md:flex md:order-1 flex-col">
+      {!allOn && (
+        <button
+          type="button"
+          onClick={onReset}
+          className="meta-link absolute right-0 -top-6"
+        >
+          Rodyti viską
+        </button>
+      )}
 
       <CategoryList
         active={active}
